b9c7af6fc158800e9666474393b0833bbb4372c5
Adivinha <samuel.pereira@multfacilcomercial.com.br>
2024-03-17 22:59:31 -0300

    criando tabela

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,17 @@
 import fastify from 'fastify'
 
-const server = fastify()
+const app = fastify({
+    logger: false
+})
 
-server.get('/', async (request, reply) => {
+app.get('/', async (request, reply) => {
   return 'e ai é dentro teste'
 })
 
-server.listen({host: '0.0.0.0', port: 3000 }, (err, address) => {
+app.listen({host: '0.0.0.0', port: 3000 }, (err, address) => {
   if (err) {
     console.error(err)
     process.exit(1)
   }
-  console.log(`Server listening at ${address}`)
+  console.log(address)
 })
