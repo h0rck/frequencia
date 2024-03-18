fc5d40d6c04486be98c19fac2c96490fc87726e3
Adivinha <samuel.pereira@multfacilcomercial.com.br>
2024-03-17 23:56:13 -0300

    criando testes

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,10 +1,12 @@
 import app from './app';
 
-test('Teste da rota GET /', async () => {
-  const response = await app.inject({
-    method: 'GET',
-    url: '/'
-  });
-  expect(response.statusCode).toBe(200);
-  expect(response.payload).toEqual('Hello, world!');
-});
+
+// test('Teste da rota GET /', async () => {
+//   const response = await app.inject({
+//     method: 'GET',
+//     url: '/'
+//   });
+//   expect(response.statusCode).toBe(200);
+//   console.log(response.payload)
+// //   expect(response.payload).toEqual('Hello, world!');
+// });
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,43 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 const app = fastify({
     logger: false
 })
 
 app.get('/', async (request, reply) => {
-  return 'e ai é dentro teste'
+  return 'É dentro'
+})
+
+// id Int @id @default(autoincrement())
+//   ra String 
+//   frequencia Int
+//   turma String
+//   turmaCodigo String
+//   escola String
+//   escolaCodigo String
+//   cidade String
+
+app.post('/frequencia', {
+    schema: {
+        body: {
+          type: 'object',
+          required: ['ra', 'frequencia', 'turma', 'turmaCodigo', 'escola', 'EscolaCOdigo', 'cidade'],
+          properties: {
+            ra: { type: 'string' }, 
+            frequencia : { type: 'int' },
+            turma : { type: 'string' }, 
+            turmaCodigo : { type: 'string' }, 
+            escola : { type: 'string' }, 
+            EscolaCOdigo : { type: 'string' }, 
+            cidade : { type: 'string' }, 
+          }
+        }
+      }
+}, async (req, res) => {
+    res.send(req)
 })
 
 app.listen({host: '0.0.0.0', port: 8085 }, (err, address) => {
